fd04b7328a227f1149a6b76d40dc21127ba872d8
Irena Chen <irena1009@gmail.com>
2021-08-12 17:22:18 -0700

    add hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,43 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = ()=> setClick(false)
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        TRVL
+                        TRVL <i className="fab fa-typo3"></i>
                     </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times': 'fas fa-bars'}/>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' calssName='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/services' calssName='nav-links' onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/products' calssName='nav-links' onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/sign-up' calssName='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sing up
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
