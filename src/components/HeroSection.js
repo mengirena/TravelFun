325e783019ebbd85fcaa60b92cf0a19c2e6d6d3a
Irena Chen <irena1009@gmail.com>
2021-08-14 16:04:04 -0700

    change hero section style

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,10 +7,10 @@ function HeroSection() {
         <div className="hero-container">
             <video src="/videos/video-2.mp4" autoPlay loop muted></video>
             <h1>ADVENTURE AWAITS</h1>
-            <P>What are you waiting for?</P>
+            <p>What are you waiting for?</p>
             <div className="hero-btns">
-                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">GET STARTED</Button>
-                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">WATCH TRAILER <i className="far fa-play-circle"></i></Button>
+                <Button className="btn" buttonStyle="btn--outline" buttonSize="btn--large">GET STARTED</Button>
+                <Button className="btn" buttonStyle="btn--primary" buttonSize="btn--large">WATCH TRAILER <i className="far fa-play-circle"></i></Button>
 
             </div>
         </div>
